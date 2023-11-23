702a66a45bc77a125d37114b422b7e8c10dd7f41
fyzdalao <60678086+fyzdalao@users.noreply.github.com>
2023-11-23 20:31:43 +0800

    11.23 at home. Naive algorithm works, but doesn't record history.

--- zoopt/algos/opt_algorithms/fyzalgorithm/naive_optimization.py
+++ zoopt/algos/opt_algorithms/fyzalgorithm/naive_optimization.py
@@ -25,25 +25,21 @@ class Naive:
         self.set_parameters(parameter)
         time_log1 = time.time()
         dim: Dimension = self._objective.get_dim()
-        x_0: Solution = self._objective.construct_solution(dim.rand_sample())
-        self._best_solution = x_0
+        self._best_solution = self._objective.construct_solution(dim.rand_sample())
         delta = 1
-        previous_value = self._objective.eval(x_0)
+        previous_value = self._objective.eval(self._best_solution)
         count = 0
         while True:
             count += 1
-            if count > 500:
-                return self._best_solution
             if self._parameter.get_time_budget() is not None:
-                if time.time() - time_log1 >= self._parameter.get_time_budget():
-                    ToolFunction.log('naive algorithm runs out of time_budget')
+                if (time.time() - time_log1) >= self._parameter.get_time_budget():
+                    ToolFunction.log('naive-algorithm runs out of time_budget')
                     return self._best_solution
-            x_t: Solution = self._objective.construct_solution(x_0.get_x()+self.generate_vector(delta))
+            x_t: Solution = self._objective.construct_solution(self.generate_vector(delta))
             new_value = self._objective.eval(x_t)
             if new_value < previous_value:
                 delta *= 1.2
-                x_0 = x_t
-                self._best_solution = x_0
+                self._best_solution = x_t
             else:
                 delta *= 0.8
 
@@ -55,6 +51,7 @@ class Naive:
         v = [random.random() for _ in range(dimsize)]
         for i in range(dimsize):
             v[i] *= (region[i][1] - region[i][0])*0.01*times
+            v[i] += self._best_solution.get_x()[i]
         return v
 
     def set_parameters(self, parameter):
